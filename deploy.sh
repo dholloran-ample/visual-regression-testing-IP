aec27deca5d6e3f94c7efe5029d5c4e968d46cd7
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-01-24 15:50:26 -0500

    Add option to skip NPM deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 if ([ "$TRAVIS" = "true" ] && [ "$TRAVIS_BRANCH" = "master" ]); then
-  npm publish --dry-run
+  if["$SKIP_NPM_DEPLOY" = "true"]; then
+    npm publish --dry-run
+  else
+    npm publish
+  fi
 fi
