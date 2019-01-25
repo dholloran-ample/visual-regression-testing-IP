1110e44a739fa3132fb29b32d5d9d5193ef85ef2
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-01-24 21:46:41 -0500

    Add Travis deployment strategy for NPM

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ([ "$TRAVIS" = "true" ] && [ "$TRAVIS_BRANCH" = "master" ]); then
-  if["$SKIP_NPM_DEPLOY" = "true"]; then
-    npm publish --dry-run
-  else
-    npm publish
-  fi
-fi
