7190ca94ea1c11161bc6f742ff7f9de2c270e76d
Warren Harrison <warren@hungry-media.com>
2019-05-20 10:28:40 -0400

    Static HTML and SCSS to match existing footer.

---
+++ src/components/shared-footer/shared-footer.tsx
@@ -0,0 +1,45 @@
+import { Component, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'shared-footer',
+  styleUrl: 'shared-footer.scss',
+  shadow: true
+})
+export class SharedFooter {
+
+  // Indicate that name should be a public property on the component
+  @Prop() name: string;
+
+  render() {
+    return (
+      <footer>
+        <div class="container">
+          <div>
+            <h5><a href="https://www.crossroads.net/locations/">One Church, Many Locations</a></h5>
+            <p><strong>Cincinnati Area</strong><br /><a href="https://www.crossroads.net/eastside/">East Side</a><a href="https://www.crossroads.net/florence/"><br />Florence</a> <br /><a href="https://www.crossroads.net/mason/">Mason</a> <br /><a href="https://www.crossroads.net/oakley/" data-automation-id="footer-oakley-location">Oakley</a><br /><a href="https://www.crossroads.net/oxford/">Oxford</a> <br /><a href="https://www.crossroads.net/uptown/">Uptown</a><br /><a href="https://www.crossroads.net/westside/">West Side</a></p>
+            <p><strong>Central Kentucky Area</strong><br /><a href="https://www.crossroads.net/lexington/" class="ss-broken ss-broken">Lexington</a><br /><a href="https://www.crossroads.net/downtownlex/" class="ss-broken ss-broken">Downtown Lexington</a><br /><a href="https://www.crossroads.net/georgetown/" class="ss-broken ss-broken">Georgetown</a><br /><a href="https://www.crossroads.net/richmond/" class="ss-broken ss-broken">Richmond</a></p>
+            <p><a href="https://www.crossroads.net/live/" class="ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken">Anywhere</a><br /><a href="https://www.crossroads.net/cleveland/">Cleveland</a><br /><a href="https://www.crossroads.net/columbus/">Columbus</a><br /><a href="https://www.crossroads.net/dayton/">Dayton</a></p>
+          </div>
+          <div>
+            <h5><a href="http://imincampaign.net/" target="_blank">I’M IN Campaign</a></h5>
+            <p><a href="mailto:campaign@crossroads.net">I'M IN questions</a><br /><a href="https://www.crossroads.net/leaveyourmark">Campaign Status</a></p>
+          </div>
+          <div>
+            <h5>Contact</h5>
+            <p><a href="https://www.crossroads.net/prayer/">Prayer Requests</a> <br /><a href="https://www.crossroads.net/shareyourstory/">Share Your Story</a> <br /><a href="https://www.crossroads.net/contactus/">Contact Us</a> <br /><a href="https://www.crossroads.net/privacypolicy/">Privacy Policy</a></p>
+          </div>
+          <div>
+            <h5>Resources</h5>
+            <p><a href="https://www.crossroads.net/inthenews/">In The News</a><br /><a href="https://www.crossroads.net/churchresources/">Church Resources</a></p>
+          </div>
+          <div class="social-icons">
+            <a href="https://twitter.com/crdschurch/" target="_blank" class="text-center"><img src="//d1tmclqz61gqwd.cloudfront.net/images/twitter.svg" alt="twitter account" title="twitter account" /></a>
+            <a href="https://www.facebook.com/crdschurch/?_rdr=p" target="_blank" class="text-center"> <img src="//d1tmclqz61gqwd.cloudfront.net/images/facebook.svg" alt="facebook account" title="facebook account" /></a>
+            <a href="https://www.youtube.com/user/crdschurch" target="_blank" class="text-center"> <img src="//d1tmclqz61gqwd.cloudfront.net/images/youtube.svg" alt="youtube account" title="youtube account" /></a>
+            <a href="https://www.instagram.com/crdschurch/" target="_blank" class="text-center"> <img src="//d1tmclqz61gqwd.cloudfront.net/images/instagram.svg" alt="instagram account" title="instagram account" /></a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
