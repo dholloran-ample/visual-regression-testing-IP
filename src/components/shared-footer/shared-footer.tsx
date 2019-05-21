32468583a2b819226e4314992659bf5e2bc902c1
Warren Harrison <warren@hungry-media.com>
2019-05-21 15:50:44 -0400

    Drive footer elements from JSON and use more semantic markup.

--- src/components/shared-footer/shared-footer.tsx
+++ src/components/shared-footer/shared-footer.tsx
@@ -1,37 +1,191 @@
 import { Component, Prop } from '@stencil/core';
+import Fragment from 'stencil-fragment';
 
 @Component({
   tag: 'shared-footer',
   styleUrl: 'shared-footer.scss',
   shadow: true
 })
+
 export class SharedFooter {
 
-  // Indicate that name should be a public property on the component
-  @Prop() name: string;
+  // TODO: Move this elsewhere
+  private nav: any = [
+    {
+      "title": "One Church, Many Locations",
+      "path": "/locations/",
+      "children": [
+        {
+          "title": "Cincinnati Area",
+          "children": [
+            {
+              "title": "East Side",
+              "path": "/eastside/"
+            },
+            {
+              "title": "Florence ",
+              "path": "/florence/"
+            },
+            {
+              "title": "Mason ",
+              "path": "/mason/"
+            },
+            {
+              "title": "Oakley",
+              "path": "/oakley/"
+            },
+            {
+              "title": "Oxford ",
+              "path": "/oxford/"
+            },
+            {
+              "title": "Uptown",
+              "path": "/uptown/"
+            },
+            {
+              "title": "West Side",
+              "path": "/westside/"
+            }
+          ]
+        },
+        {
+          "title": "Central Kentucky Area",
+          "children": [
+            {
+              "title": "Lexington",
+              "path": "/lexington/"
+            },
+            {
+              "title": "Downtown Lexington",
+              "path": "/downtownlex/"
+            },
+            {
+              "title": "Georgetown",
+              "path": "/georgetown/"
+            },
+            {
+              "title": "Richmond",
+              "path": "/richmond/"
+            }
+          ]
+        },
+        {
+          "children": [
+            {
+              "title": "Anywhere",
+              "path": "/live/"
+            },
+            {
+              "title": "Cleveland",
+              "path": "/cleveland/"
+            },
+            {
+              "title": "Columbus",
+              "path": "/columbus/"
+            },
+            {
+              "title": "Dayton",
+              "path": "/dayton/"
+            },
+          ]
+        }
+      ]
+    },
+    {
+      "title": "I’M IN Campaign",
+      "path": "http://imincampaign.net/",
+      "children": [
+        {
+          "title": "I'M IN questions",
+          "path": "mailto:campaign@crossroads.net"
+        },
+        {
+          "title": "Campaign Status",
+          "path": "/leaveyourmark"
+        },
+      ]
+    },
+    {
+      "title": "Contact",
+      "children": [
+        {
+          "title": "Prayer Requests",
+          "path": "/prayer/"
+        },
+        {
+          "title": "Share Your Story",
+          "path": "/shareyourstory/"
+        },
+        {
+          "title": "Contact Us",
+          "path": "/contactus/"
+        },
+        {
+          "title": "Privacy Policy",
+          "path": "/privacypolicy/"
+        }
+      ]
+    },
+    {
+      "title": "Resources",
+      "children": [
+        {
+          "title": "In The News",
+          "path": "/inthenews/"
+        },
+        {
+          "title": "Church Resources",
+          "path": "/churchresources/"
+        }
+      ]
+    }
+  ];
 
-  render() {
+  private renderElement(el) {
+    if (el.path) {
+      let target = ''
+      if (el.path.match(/:\/\//)) { target = '_blank' }
+      return <a target={target} href={el.path}>{el.title}</a>
+    } else {
+      return el.title
+    }
+  }
+
+  private renderGroups(groups) {
+    let hasChildren = false
+    const groupMarkup = groups.map(group => {
+      if (group.children) {
+        hasChildren = true
+        return (
+          <fragment>
+            <p>{group.title}</p>
+            <ul>
+              {group.children.map(el => <li>{this.renderElement(el)}</li>)}
+            </ul>
+          </fragment>
+        )
+      } else {
+        return <li>{this.renderElement(group)}</li>
+      }
+    });
+    if (hasChildren) {
+      return groupMarkup
+    } else {
+      return <ul>{groupMarkup}</ul>
+    }
+  }
+
+  public render() {
     return (
       <footer>
         <div class="container">
+        {this.nav.map((column) =>
           <div>
-            <h5><a href="https://www.crossroads.net/locations/">One Church, Many Locations</a></h5>
-            <p><strong>Cincinnati Area</strong><br /><a href="https://www.crossroads.net/eastside/">East Side</a><a href="https://www.crossroads.net/florence/"><br />Florence</a> <br /><a href="https://www.crossroads.net/mason/">Mason</a> <br /><a href="https://www.crossroads.net/oakley/" data-automation-id="footer-oakley-location">Oakley</a><br /><a href="https://www.crossroads.net/oxford/">Oxford</a> <br /><a href="https://www.crossroads.net/uptown/">Uptown</a><br /><a href="https://www.crossroads.net/westside/">West Side</a></p>
-            <p><strong>Central Kentucky Area</strong><br /><a href="https://www.crossroads.net/lexington/" class="ss-broken ss-broken">Lexington</a><br /><a href="https://www.crossroads.net/downtownlex/" class="ss-broken ss-broken">Downtown Lexington</a><br /><a href="https://www.crossroads.net/georgetown/" class="ss-broken ss-broken">Georgetown</a><br /><a href="https://www.crossroads.net/richmond/" class="ss-broken ss-broken">Richmond</a></p>
-            <p><a href="https://www.crossroads.net/live/" class="ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken ss-broken">Anywhere</a><br /><a href="https://www.crossroads.net/cleveland/">Cleveland</a><br /><a href="https://www.crossroads.net/columbus/">Columbus</a><br /><a href="https://www.crossroads.net/dayton/">Dayton</a></p>
+            <h5>{this.renderElement(column)}</h5>
+            {this.renderGroups(column.children)}
           </div>
-          <div>
-            <h5><a href="http://imincampaign.net/" target="_blank">I’M IN Campaign</a></h5>
-            <p><a href="mailto:campaign@crossroads.net">I'M IN questions</a><br /><a href="https://www.crossroads.net/leaveyourmark">Campaign Status</a></p>
-          </div>
-          <div>
-            <h5>Contact</h5>
-            <p><a href="https://www.crossroads.net/prayer/">Prayer Requests</a> <br /><a href="https://www.crossroads.net/shareyourstory/">Share Your Story</a> <br /><a href="https://www.crossroads.net/contactus/">Contact Us</a> <br /><a href="https://www.crossroads.net/privacypolicy/">Privacy Policy</a></p>
-          </div>
-          <div>
-            <h5>Resources</h5>
-            <p><a href="https://www.crossroads.net/inthenews/">In The News</a><br /><a href="https://www.crossroads.net/churchresources/">Church Resources</a></p>
-          </div>
+        )}
+
           <div class="social-icons">
             <a href="https://twitter.com/crdschurch/" target="_blank" class="text-center"><img src="//d1tmclqz61gqwd.cloudfront.net/images/twitter.svg" alt="twitter account" title="twitter account" /></a>
             <a href="https://www.facebook.com/crdschurch/?_rdr=p" target="_blank" class="text-center"> <img src="//d1tmclqz61gqwd.cloudfront.net/images/facebook.svg" alt="facebook account" title="facebook account" /></a>
