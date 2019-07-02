70d6db73b524dc0a19570b32d9282ba77114a158
Sean C Davis <scdavis41@gmail.com>
2019-07-02 14:42:35 -0400

    DEXXXX - Profile Picture Env Make subdomain map a utility and fix issue with profile image url

--- src/shared/auth.ts
+++ src/shared/auth.ts
@@ -92,7 +92,7 @@ export class Auth {
     if (!this.authenticated) return null;
     const userId = this.getUserId();
     if (!userId) return null;
-    const subdomain = this.subdomainMap[this.config.env] || this.config.env;
+    const subdomain = Utils.getSubdomain(this.config.env);
     return `https://${subdomain}.crossroads.net/proxy/gateway/api/image/profile/${userId}`;
   }
 }
