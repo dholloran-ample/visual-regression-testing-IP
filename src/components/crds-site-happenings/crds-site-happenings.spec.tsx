ba71f6ee7cb92854d2c8261cdb3e299159f0fe81
Sarah Henney <shenney@callibrity.com>
2019-08-07 17:26:28 -0400

    Extending site happenings unit tests

--- src/components/crds-site-happenings/crds-site-happenings.spec.tsx
+++ src/components/crds-site-happenings/crds-site-happenings.spec.tsx
@@ -8,4 +8,29 @@ describe('<crds-site-happenings>', () => {
   it('should default to Churchwide for logged out users', () => {
     expect(this.happenings.selectedSite).toEqual('Churchwide');
   });
+
+  describe('Tests defaultToUserSite()', () => {
+    it('should display Churchwide if user missing site', () => {
+      const flaggedSites = ['Not site specific', 'I do not attend Crossroads', 'Anywhere', null, undefined]
+      flaggedSites.forEach(site => {
+        this.happenings.defaultToUserSite(site);
+        expect(this.happenings.selectedSite).toEqual('Churchwide');
+      });
+    });
+
+    it('should default to user site', () => {
+      const userSites = ['Oakley', 'Downtown Lexington', 'Fake Site', ['Array Site'], {name: 'Object Site'}]
+      userSites.forEach(site => {
+        this.happenings.defaultToUserSite(site);
+        expect(this.happenings.selectedSite).toEqual(site);
+      });
+    });
+  });
+
+
+  //These use defaultToUserSite:
+  //fetchUserData, handleSetDefaultSite
+  //change filter - check happenings list lists promos?
+
+
 });
