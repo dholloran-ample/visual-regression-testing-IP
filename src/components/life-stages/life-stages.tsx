0f9133dc0e0f95805daa6ffae843c2aa2ad8de1f
Luke Moderwell <luke.moderwell@gmail.com>
2019-08-15 16:37:50 -0400

    basic render strategy

--- src/components/life-stages/life-stages.tsx
+++ src/components/life-stages/life-stages.tsx
@@ -1,6 +1,7 @@
 import { Component, Prop, State, Element, h, Watch } from '@stencil/core';
 import { CrdsUser, CrdsLifeStage } from './life-stages-interface';
 import axios from 'axios';
+import { HTMLStencilElement } from '@stencil/core/internal';
 
 @Component({
   tag: 'life-stages',
@@ -9,34 +10,39 @@ import axios from 'axios';
 })
 export class LifeStages {
   private gqlUrl = process.env.CRDS_GQL_ENDPOINT;
-  private CrdsUser: CrdsUser = { name: '', lifeStage: '' };
-  private lifeStageContent: [] = [];
+  private user: CrdsUser = { name: '', lifeStage: '' };
+  private recommendedContent: [] = [];
   private lifeStages: CrdsLifeStage[] = [];
 
   @Prop() authToken: string;
   @State() selectedStage: string = '';
-  @Element() host: HTMLElement;
+  @Element() host: HTMLStencilElement;
 
   @Watch('authToken')
   authTokenHandler() {
-    this.fetchUserLifeStage();
+    this.fetchUser();
   }
 
   componentWillLoad() {
     // actually need to do a fetch to cosmosDB first
     // if user already has life stage we just need that single life stages data
-    return Promise.all([this.fetchUserLifeStage(), this.fetchLifeStages()]);
+    return Promise.all([this.fetchUser(), this.fetchLifeStages()]);
   }
 
-  private fetchUserLifeStage() {
+  private refresh() {
+    console.log('re-rendering', this.recommendedContent, this.lifeStages);
+    this.host.forceUpdate();
+  }
+
+  private fetchUser() {
     // reach into cosmos db with user identifier and see if they already have a life stage
-    console.log("fetching user's life stage!");
+    console.log("fetching user info!");
   }
 
   fetchLifeStages() {
     let apiUrl = `https://graphql.contentful.com/content/v1/spaces/${
       process.env.CONTENTFUL_SPACE_ID
-    }/environments/${process.env.CONTENTFUL_ENV || 'master'}`;
+      }/environments/${process.env.CONTENTFUL_ENV || 'master'}`;
     return axios
       .get(apiUrl, {
         params: {
@@ -59,6 +65,7 @@ export class LifeStages {
       })
       .then(success => {
         this.lifeStages = success.data.data.lifeStageCollection.items;
+        this.refresh();
       });
   }
 
@@ -68,7 +75,7 @@ export class LifeStages {
   fetchContent() {
     let apiUrl = `https://graphql.contentful.com/content/v1/spaces/${
       process.env.CONTENTFUL_SPACE_ID
-    }/environments/${process.env.CONTENTFUL_ENV || 'master'}`;
+      }/environments/${process.env.CONTENTFUL_ENV || 'master'}`;
     return axios
       .get(apiUrl, {
         params: {
@@ -91,74 +98,66 @@ export class LifeStages {
       .then(success => {
         let articles = success.data.data.articleCollection.items;
         // faking the content here
-        this.lifeStageContent = articles.filter(item => item.duration > 550).slice(1, 3);
-        this.lifeStages = [];
-        this.renderCards();
+        this.recommendedContent = articles.filter(item => item.title !== '').slice(1, 3);
+        this.refresh();
       })
       .catch(err => console.error(err));
   }
 
   handleLifeStageClicked(event) {
     const card = event.target;
-    card.classList.toggle('selected');
-    if (this.selectedStage === card.dataset.lifeStage) {
-      this.selectedStage = '';
-    } else {
-      this.selectedStage = card.dataset.lifeStage;
-    }
-    // TODO: analytics call here
-  }
-
-  handleConfirmClick(event) {
+    this.selectedStage = card.dataset.lifeStage;
+    this.user = { ...this.user, lifeStage: this.selectedStage };
+    // TODO: call cosmos and update user
     this.fetchContent();
-    event;
     // TODO: analytics call here
   }
 
-  renderCards() {
-    console.log('rendering cards', this.lifeStageContent, this.lifeStages);
-    const parent = this.host.shadowRoot.querySelector('.cards');
-    let cards: any = this.skeletonCards;
-    if (this.lifeStages !== []) {
-      cards = this.lifeStageCards();
-    }  else {
-      cards = this.contentCards();
-    }
-    return cards;
-  }
-
-  skeletonCards() {
-    return ([1,2,3].map(item => <div class="skeleton-card" key={item}><h1>Loading</h1></div>));
-  }
-
-  lifeStageCards() {
-    return this.lifeStages.map((obj, index) => (
-      <div
-        class={`card ${this.selectedStage == obj.title ? 'selected' : ''}`}
-        key={index}
-        style={{
-          bakgroundImage: `url(${obj.image.url})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        }}
-        data-life-stage={obj.title}
-        onClick={event => this.handleLifeStageClicked(event)}
-      >
-        <div class="bg-overlay" />
-        <div class="card-content">
-          <h2 class="card-title component-header">{obj.title}</h2>
-          <h3 class="card-subtitle">{`${obj.contentCollection.total} items`}</h3>
+  renderSkeletonCards() {
+    return ([1, 2, 3]
+      .map(item =>
+        <div
+          class={`skeleton-card ${this.lifeStages.length > 0 ? 'fade d-none' : ''}`}
+          key={item}>
+          <h1>Loading</h1>
         </div>
-      </div>
-    ));
+      )
+    );
   }
 
-  contentCards() {
-    return this.lifeStageContent.map((obj: any, index) => {
-      <div class="card" key={index}>
-        {obj.title}
-      </div>;
-    });
+  renderLifeStageCards() {
+    return (
+      this.lifeStages.map((obj, index) => (
+        <div
+          class={`card ${this.recommendedContent.length > 0 ? 'fade d-none' : ''}`}
+          key={index}
+          style={{
+            bakgroundImage: `url(${obj.image.url})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}
+          data-life-stage={obj.title}
+          onClick={event => this.handleLifeStageClicked(event)}
+        >
+          <div class="bg-overlay" />
+          <div class="card-content">
+            <h2 class="card-title component-header">{obj.title}</h2>
+            <h3 class="card-subtitle">{`${obj.contentCollection.total} items`}</h3>
+          </div>
+        </div>
+      ))
+    )
+  }
+
+  renderContentCards() {
+    console.log('rendering cards');
+    return (
+      this.recommendedContent.map((obj: any, index) => (
+        <div class="card" key={obj.title}>
+          {obj.title}
+        </div>
+      ))
+    )
   }
 
   render() {
@@ -170,9 +169,11 @@ export class LifeStages {
             <p>Which of these best describes your stage of life? (Pick one)</p>
           </div>
           <div class="cards">
-            {this.renderCards()}
+            {this.renderSkeletonCards()}
+            {this.recommendedContent.length > 0 ? this.renderContentCards() : ''}
+            {this.lifeStages.length > 0 ? this.renderLifeStageCards() : ''}
           </div>
-          <button
+          {/* <button
             class="confirm-btn"
             onClick={event => this.handleConfirmClick(event)}
             style={{
@@ -181,7 +182,7 @@ export class LifeStages {
             }}
           >
             Next
-          </button>
+          </button> */}
         </div>
       </div>
     );
