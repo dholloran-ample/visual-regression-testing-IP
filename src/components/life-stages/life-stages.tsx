15bf073aac7929b416704e2b34baca4dc504d7f5
Luke Moderwell <luke.moderwell@gmail.com>
2019-08-29 13:56:46 -0400

    remove try/catch on analytics

--- src/components/life-stages/life-stages.tsx
+++ src/components/life-stages/life-stages.tsx
@@ -179,19 +179,18 @@ export class LifeStages {
     cards.forEach(card => card.classList.add('disabled'));
     this.user.lifeStage.id = card.dataset.lifeStageId;
     this.user.lifeStage.title = card.dataset.lifeStageName;
-    try {
-      this.analytics.track('LifeStageUpdated', {
-        event: event,
-        lifeStageId: this.user.lifeStage.id,
-        lifeStageName: this.user.lifeStage.title
-      });
-    } catch (error) {
-      console.error(error);
-    }
+
+    this.analytics.track('LifeStageUpdated', {
+      event: event,
+      lifeStageId: this.user.lifeStage.id,
+      lifeStageName: this.user.lifeStage.title
+    });
+
     this.fetchContent(this.authToken, this.user.lifeStage.id).then(() => {
       card.parentNode.scrollLeft = 0;
       return cards.forEach(card => card.classList.remove('disabled'));
     });
+    
     this.setLifeStage(this.authToken, this.user.lifeStage.id, this.user.lifeStage.title);
   }
 
