d800a2907859c39619a886f5e0229a6825e363b2
Luke Moderwell <luke.moderwell@gmail.com>
2019-09-03 14:24:04 -0400

    add animated fade in

--- src/components/crds-greeting/crds-greeting.tsx
+++ src/components/crds-greeting/crds-greeting.tsx
@@ -1,5 +1,6 @@
-import { Component, Prop, State, h } from '@stencil/core';
+import { Component, Prop, State, Element, Watch, h } from '@stencil/core';
 import { GreetingUser } from './crds-greeting-interface';
+import { HTMLStencilElement } from '@stencil/core/internal';
 import { GET_NAMES } from './crds-greeting.graphql';
 import ApolloClient from 'apollo-client';
 import { CrdsApollo } from '../../shared/apollo';
@@ -20,10 +21,25 @@ export class CrdsGreeting {
   };
   @Prop() authToken: string;
   @Prop() defaultName: string;
+  @Element() public host: HTMLStencilElement;
 
-  public componentWillLoad() {
-    this.apolloClient = CrdsApollo(this.authToken);
-    return this.getUserName()
+  @Watch('authToken')
+  authTokenHandler(newValue: string, oldValue: string) {
+    if (newValue !== oldValue) {
+      this.apolloClient = CrdsApollo(newValue);
+      this.getUserName();
+    }
+  }
+
+  public componentDidRender() {
+    const renderedEvent = new CustomEvent('component rendered', {
+      detail: this.host
+    });
+    document.dispatchEvent(renderedEvent);
+
+    setTimeout(() => {
+      this.host.shadowRoot.querySelector('.greeting').classList.add('fade-in');
+    }, 0);
   }
 
   public getUserName() {
@@ -38,20 +54,14 @@ export class CrdsGreeting {
   }
 
   public parseTimeBasedGreetings(hour) {
-    let greetingText;
-    if (hour < 12) {
-      greetingText = 'Good morning';
-    } else if (hour >= 12) {
-      greetingText = 'Good afternoon' 
-    } else if (hour >= 17) {
-      greetingText = 'Good evening'
-    }
-    return greetingText;
+    if (hour >= 17) return 'Good evening';
+    if (hour >= 12) return 'Good afternoon';
+    return 'Good morning';
   }
 
   public renderGreeting() {
-    const time = new Date();
-    const greeting = this.parseTimeBasedGreetings(time);
+    const date = new Date();
+    const greeting = this.parseTimeBasedGreetings(date.getHours());
     const name = this.user.contact.nickName || this.user.contact.firstName || this.defaultName;
     return `${greeting}, ${name}`;
   }
@@ -62,7 +72,7 @@ export class CrdsGreeting {
 
   public render() {
     return (
-      <div class="push-bottom">
+      <div class="greeting">
         <h3 class="font-size-large flush">{this.renderGreeting()}</h3>
         <p class="flush">This place was made for you!</p>
       </div>
