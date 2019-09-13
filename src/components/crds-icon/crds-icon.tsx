4e395b68bc17a8465915d543d7de86698c84f6ea
Josh Byron <joshhbyron@gmail.com>
2019-09-13 11:45:16 -0400

    US17970-media-card - Adds crds-icon

---
+++ src/components/crds-icon/crds-icon.tsx
@@ -0,0 +1,33 @@
+import { Component, Prop, h} from '@stencil/core';
+
+// icons
+import icons from './icons.svg';
+import colors from './crds-icon-colors.js';
+
+@Component({
+    tag: 'crds-icon',
+    styleUrl: 'crds-icon.scss',
+    shadow: true
+})
+
+export class CrdsIcon {
+
+    @Prop() name: string;
+    @Prop() color: string;
+    @Prop() size: string;
+
+    render() {
+
+        const domparser = new DOMParser();
+        const doc = domparser.parseFromString(icons, 'text/html');
+        const svg = doc.getElementById(this.name);
+        
+        svg.setAttribute('height',`${Number(this.size) * 1.3333}px`);
+        svg.setAttribute('width',`${this.size}px`);
+        svg.getElementsByTagName('path')[0].setAttribute('fill', colors[this.color]);
+
+        return (
+            <div class='svg-container' innerHTML={svg.outerHTML} />
+        );
+    }
+}
