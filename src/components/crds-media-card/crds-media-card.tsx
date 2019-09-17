e017476ebcd96f794d50b89973fd4e0a21c270c9
Josh Byron <joshhbyron@gmail.com>
2019-09-17 13:37:31 -0400

    US17970-media-card - Adds additional validations - Adjust prop names

--- src/components/crds-media-card/crds-media-card.tsx
+++ src/components/crds-media-card/crds-media-card.tsx
@@ -1,5 +1,5 @@
 // Stencil
-import { Component, Prop, Element, h, State, Watch } from '@stencil/core';
+import { Component, Prop, Element, h, State } from '@stencil/core';
 
 @Component({
   tag: 'crds-media-card',
@@ -7,18 +7,20 @@ import { Component, Prop, Element, h, State, Watch } from '@stencil/core';
   shadow: true
 })
 export class CrdsMediaCard {
+  // ----------------------------------------------- | Instance Variables
+
   // Content
   @Prop() contentType: string;
   @Prop() contentLayout: string = 'default';
 
   // Default props
-  @Prop() image: string;
+  @Prop() imageSrc: string;
   @Prop() heading: string;
   @Prop() meta: string;
   @Prop() metaPosition: string;
-  @Prop() body: string;
-  @Prop() src: string;
-  @Prop() thumbnailSRC: string;
+  @Prop({ reflect: false }) body: string;
+  @Prop() buttonSrc: string;
+  @Prop() thumbnailSrc: string;
   @Prop() url: string;
 
   // article / video Podcast episode, message, song props
@@ -33,53 +35,69 @@ export class CrdsMediaCard {
   @State() isVisible: boolean = false;
   @State() childProps = {};
 
-  @Element() element: HTMLElement;
-
-  // Validations
-  @Watch('contentType')
-  validateContentType(newValue: string) {
-    if (typeof newValue != null && this.contentTypes.indexOf(newValue) == -1) {
-      throw new Error(`${newValue} is not a valid value for contentType`);
-    }
-  }
-
-  @Watch('contenLayout')
-  validateContentLayout(newValue: string) {
-    if (typeof newValue != null && this.contentLayouts.indexOf(newValue) == -1) {
-      throw new Error(`${newValue} is not a valid value for contentLayout`);
-    }
-  }
-
-  @Watch('image')
-  validateName(newValue: string) {
-    const isEmpty = typeof newValue == null;
-    if (isEmpty) {
-      throw new Error('image property is required on all media cards');
-    }
-  }
-
   private propNames = [
-    'image',
+    'imageSrc',
     'heading',
-    'subtitle',
-    'metadata',
-    'description',
-    'buttonHREF',
+    'meta',
+    'metaPosition',
+    'body',
+    'url',
+    'buttonSrc',
     'nearestMinute',
     'author',
     'mediaTopic',
-    'thumbImageHREF',
-    'contentCount'
+    'thumbnailSrc',
+    'contentCount',
+    'contentType'
   ];
 
   private contentLayouts = ['default', 'overlay', 'media-object'];
   private contentTypes = ['article', 'video', 'podcast-episode', 'message', 'song', 'series', 'album', 'podcast'];
+  private metaPositions = ['top', 'bottom'];
+
+  // ----------------------------------------------- | Validations
+  private validateContentType() {
+    if (typeof this.contentType != 'undefined' && this.contentTypes.indexOf(this.contentType) == -1) {
+      throw new Error(`${this.contentType} is not a valid value for contentType`);
+    }
+  }
+
+  private validateContentLayout() {
+    if (typeof this.contentLayout != 'undefined' && this.contentLayouts.indexOf(this.contentLayout) == -1) {
+      throw new Error(`${this.contentLayout} is not a valid value for contentLayout`);
+    }
+  }
+
+  private validateImage() {
+    const isEmpty = typeof this.imageSrc == 'undefined';
+    if (isEmpty) {
+      throw new Error('imageSrc property is required on all media cards');
+    }
+  }
+
+  private validateMeta() {
+    if (typeof this.meta != 'undefined' && typeof this.metaPosition == 'undefined') {
+      throw new Error(`metaPosition is required if you want to provide a meta`);
+    }
+  }
+
+  private validateMetaPosition() {
+    if (typeof this.metaPosition != 'undefined' && this.metaPositions.indexOf(this.metaPosition) == -1) {
+      throw new Error(`${this.metaPosition} is not a valid value for metaPosition`);
+    } else if (typeof this.meta == 'undefined' && typeof this.metaPosition != 'undefined') {
+      throw new Error(`Meta is required if you want to provide a meta posittion`);
+    }
+  }
 
   // ----------------------------------------------- | Methods
 
   componentWillLoad() {
     // Set props for child component
-    this.childProps = this.propNames.filter(prop => this[prop] != undefined);
+    this.propNames
+      .filter(prop => this[prop] != undefined)
+      .forEach(prop => {
+        this.childProps[prop] = this[prop];
+      });
   }
 
   private getLayout = () => {
@@ -89,6 +107,18 @@ export class CrdsMediaCard {
       'media-object': <crds-media-object-layout {...this.childProps} />
     }[this.contentLayout];
   };
+
+  private runValidations() {
+    this.validateImage();
+    this.validateContentLayout();
+    this.validateContentType();
+    this.validateMeta();
+    this.validateMetaPosition();
+  }
+
+  connectedCallback() {
+    this.runValidations();
+  }
 
   public render() {
     return this.getLayout();
