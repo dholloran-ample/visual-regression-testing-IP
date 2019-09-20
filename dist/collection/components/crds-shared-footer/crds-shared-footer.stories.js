4b5412ec4319385b67386b33c300edd3ea08be9c
Josh <36064836+J-Byron@users.noreply.github.com>
2019-09-20 11:31:03 -0400

    Revert "removed dist from git tracking, added it .gitignore"

---
+++ dist/collection/components/crds-shared-footer/crds-shared-footer.stories.js
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/polymer';
+
+const stories = storiesOf('Footer', module);
+stories.add('<crds-shared-footer />', () => {
+  return `<crds-shared-footer></crds-shared-footer>`;
+});
---
+++ dist/types/components/crds-subscribe/crds-subscribe.d.ts
@@ -0,0 +1,10 @@
+export declare class CrdsSubscribe {
+    label: string;
+    src: string;
+    modalIsShowing: boolean;
+    private frame;
+    componentDidUpdate(): void;
+    handleSubscribeClick: () => void;
+    handleModalClose: () => void;
+    render(): any;
+}
