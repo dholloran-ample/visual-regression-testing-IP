5605aa22f53a04e38d5d62694d7419fc369fd7d7
Jimmy Janssen <jjanssen@csmbp0010-jj.lan>
2019-10-11 11:57:46 -0400

    fixing broken tests from new render delegation

--- src/components/crds-greeting/tests/crds-greeting-render.spec.tsx
+++ src/components/crds-greeting/tests/crds-greeting-render.spec.tsx
@@ -10,21 +10,21 @@ describe('<greeting-component> Render', () => {
     it('displays default name if no user is found', () => {
       this.greeting.user = { nickname: null, firstName: null };
       this.greeting.getDisplayName();
-      const greet = this.greeting.renderGreeting();
+      const greet = this.greeting.renderName();
       expect(greet).toContain('Boris');
     });
 
     it('does display nickname when user nickname is valid', () => {
       this.greeting.user = { firstName: 'first', nickName: 'nick' };
       this.greeting.getDisplayName();
-      const greet = this.greeting.renderGreeting();
+      const greet = this.greeting.renderName();
       expect(greet).toContain('nick');
     });
 
     it('does display first name when user nickname is invalid', () => {
       this.greeting.user = { nickName: null, firstName: 'first' };
       this.greeting.getDisplayName();
-      const greet = this.greeting.renderGreeting();
+      const greet = this.greeting.renderName();
       expect(greet).toContain('first');
     });
 
