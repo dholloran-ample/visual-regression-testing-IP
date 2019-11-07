2c7114219b5563a25517f054b3a278aff064b0ec
jakecatron <jake.catron@crossroads.net>
2019-11-07 13:01:47 -0500

    adding .stories.js for 'crds-image-title-cutout'

---
+++ src/components/crds-image-title-cutout/crds-image-title-cutout.stories.js
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/polymer';
+
+
+const stories = storiesOf('Personalization', module);
+stories
+  .add('<crds-image-title-cutout />', () => {
+    return `<crds-image-title-cutout></crds-image-title-cutout>`;
+  });
