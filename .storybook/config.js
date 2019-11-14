fb69417393f580e32d16d2bb1875ee0b816f4a87
jakecatron <jake.catron@crossroads.net>
2019-11-14 10:59:45 -0500

    resetting storybook config'

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,14 +2,6 @@ import { configure } from "@storybook/polymer";
 
 const req = require.context("../src", true, /\.stories\.js$/);
 function loadStories() {
-  req.keys().forEach(filename =>{
-   
-    if(filename === "./components/crds-image-title-cutout/crds-image-title-cutout.stories.js") {
-      req(filename);
-    }
-
-   
-
-  } );
+  req.keys().forEach(filename => req(filename));
 }
 configure(loadStories, module);
