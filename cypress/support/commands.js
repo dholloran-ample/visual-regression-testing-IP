4616882f28947009880c2d6fb4caa2d5a15e0156
Lynch <llynch@callibrity.com>
2019-11-18 09:31:21 -0500

    update ninety day tithe challenge test

---
+++ cypress/support/commands.js
@@ -0,0 +1,32 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+import { addCommandLogin } from 'crds-cypress-login';   
+
+
+//OR if CRDS_ENV is not set:
+//OR to set the login domain if baseUrl should not be used or is not set:
+addCommandLogin('int', 'https://int.crossroads.net/')
