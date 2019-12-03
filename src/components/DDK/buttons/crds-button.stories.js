60cf9ba4e8d05ed7c7ae96f4ebf0d9c8a65ed486
j-brammer <jbrammer@callibrity.com>
2019-12-03 10:10:00 -0500

    DE7340 removed input type removed types from story added onclick to story classes are populated from method

--- src/components/DDK/buttons/crds-button.stories.js
+++ src/components/DDK/buttons/crds-button.stories.js
@@ -1,32 +1,30 @@
 import { storiesOf } from '@storybook/polymer';
 import { withKnobs, select, boolean, text } from '@storybook/addon-knobs';
-import { colors, sizes, displays, types } from './crds-button-options';
+import { colors, sizes, displays } from './crds-button-options';
 
 const stories = storiesOf('DDK/Atoms', module);
 stories.addDecorator(withKnobs).add('crds-button', () => {
-  const type = select('type', types, '');
-  const color = select('color', colors, type == 'link' ? '' : 'blue');
+  const color = select('color', colors, 'blue');
   const buttonText = text('text', 'Button');
-  const value = text('value', '');
   const href = text('href', '');
   const size = select('size', sizes, '');
   const display = select('display', displays, '');
   const disabled = boolean('disabled', false);
   const secondary = boolean('secondary', false);
   const block = boolean('block', false);
+  const onClick = text('onClick', 'alert(\'Button Pressed\')');
 
   document.getElementById('root').parentElement.parentElement.style.backgroundColor = color === 'white' ? 'black' : '';
 
   return `<crds-button 
-              ${color && type !== 'link' ? 'color=' + color : ''}
+              ${color ? 'color=' + color : ''}
               ${buttonText ? 'text=' + buttonText.replace(/\s/g, '&nbsp;') : ''}
               ${href ? 'href=' + href : ''}
-              ${value ? 'value=' + value : ''}
-              ${type ? 'type=' + type : ''} 
               ${size ? 'size=' + size : ''}  
               ${display ? 'display=' + display : ''} 
               ${disabled ? 'disabled=' + disabled : ''} 
               ${secondary ? 'secondary=' + secondary : ''} 
               ${block ? 'block=' + block : ''}
+              ${onClick ? 'onClick=' + onClick.replace(/\s/g, '&nbsp;') : ''}
               ></crds-button>`;
 });
