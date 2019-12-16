2c069585ebe7c2d83bbf282126be275f9a577d8e
Angelica Winkler <angelicawinkler@Angelicas-MacBook-Pro.local>
2019-12-16 16:02:33 -0500

    successfully passing props to crds-icon

--- src/components/DDK/buttons/crds-button.stories.js
+++ src/components/DDK/buttons/crds-button.stories.js
@@ -1,6 +1,9 @@
 import { storiesOf } from '@storybook/polymer';
 import { withKnobs, select, boolean, text } from '@storybook/addon-knobs';
 import { colors, sizes, displays } from './crds-button-options';
+import icons from '../../crds-icon/crds-icon-names'
+import iconColors from '../../../shared/crds-icon-colors'
+
 
 const stories = storiesOf('DDK/Atoms', module);
 stories.addDecorator(withKnobs).add('crds-button', () => {
@@ -13,18 +16,31 @@ stories.addDecorator(withKnobs).add('crds-button', () => {
   const secondary = boolean('secondary', false);
   const block = boolean('block', false);
   const onClick = text('onClick', 'alert(\'Button Pressed\')');
+  const icon = select('icon', icons, 'camera');
+  const iconSizeOptions = { '12': '12', '24': '24', '36': '36' };
+  const iconSize = select('size(px)', iconSizeOptions, '12');
+  const iconColor = select('iconColor', Object.keys(iconColors), 'black')
+
+  const iconAlignOptions = ['left', 'right'];
+  const iconAlign = select('iconAlign', iconAlignOptions, 'left');
+
+
 
   document.getElementById('root').parentElement.parentElement.style.backgroundColor = color === 'white' ? 'black' : '';
 
-  return `<crds-button 
+  return `<crds-button
               ${color ? 'color=' + color : ''}
               ${buttonText ? 'text=' + buttonText.replace(/\s/g, '&nbsp;') : ''}
               ${href ? 'href=' + href : ''}
-              ${size ? 'size=' + size : ''}  
-              ${display ? 'display=' + display : ''} 
-              ${disabled ? 'disabled=' + disabled : ''} 
-              ${secondary ? 'secondary=' + secondary : ''} 
+              ${size ? 'size=' + size : ''}
+              ${display ? 'display=' + display : ''}
+              ${disabled ? 'disabled=' + disabled : ''}
+              ${secondary ? 'secondary=' + secondary : ''}
               ${block ? 'block=' + block : ''}
               ${onClick ? 'onClick=' + onClick.replace(/\s/g, '&nbsp;') : ''}
+              ${icon ? 'icon=' + icon : ''}
+              ${iconSize ? 'icon-size=' + iconSize : ''}
+              ${iconColor ? 'icon-color=' + iconColor : ''}
+              ${iconAlign ? 'icon-align=' + iconAlign : ''}
               ></crds-button>`;
 });
--- src/components/DDK/buttons/crds-button.tsx
+++ src/components/DDK/buttons/crds-button.tsx
@@ -15,6 +15,10 @@ export class CrdsPrimaryButton {
   @Prop() disabled: boolean;
   @Prop() secondary: boolean;
   @Prop() block: boolean;
+  @Prop() icon: string;
+  @Prop() iconColor: string;
+  @Prop() iconSize: string;
+  @Prop() iconAlign: string;
 
   private getClasses() {
     if (this.display === 'link') return 'btn btn-link' + (this.secondary ? ` secondary` : '');
@@ -22,6 +26,7 @@ export class CrdsPrimaryButton {
       'btn' +
       (this.color ? ` btn-${this.color}` : '') +
       (this.size ? ` btn-${this.size}` : '') +
+      (this.iconAlign ? ` icon-align-${this.iconAlign}` : '') +
       (this.display ? ` btn-${this.display}` : '') +
       (this.block ? ` btn-block` : '')
     );
@@ -35,7 +40,12 @@ export class CrdsPrimaryButton {
         onClick={this.onClick}
         type="button"
       >
-        {this.text}
+        <span>
+          {this.icon && this.iconAlign === 'left' ? this.renderIcon(this.icon, this.iconSize, this.iconColor) : ''}
+          {this.text}
+          {this.icon && this.iconAlign === 'right' ? this.renderIcon(this.icon, this.iconSize, this.iconColor) : ''}
+
+        </span>
       </button>
     );
   }
@@ -47,6 +57,11 @@ export class CrdsPrimaryButton {
       </a>
     );
   }
+
+  private renderIcon(icon, size, color){
+    return <crds-icon name={icon} size={size} color={color}></crds-icon>
+  }
+
 
   public render() {
     if (this.href) return this.renderLink();
