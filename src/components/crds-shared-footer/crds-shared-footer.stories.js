ca3c0b7b7718a8ba60f4fa8aa27ddd66e1fc7b7c
Dan Holloran <dan@helloample.com>
2020-01-24 12:57:20 -0500

    Adds static footer story

--- src/components/crds-shared-footer/crds-shared-footer.stories.js
+++ src/components/crds-shared-footer/crds-shared-footer.stories.js
@@ -1,6 +1,11 @@
 import { storiesOf } from '@storybook/polymer';
+import staticFooter from './static-footer';
 
 const stories = storiesOf('Footer', module);
 stories.add('<crds-shared-footer />', () => {
   return `<crds-shared-footer></crds-shared-footer>`;
+});
+
+stories.add('Static Footer', () => {
+  return staticFooter();
 });
