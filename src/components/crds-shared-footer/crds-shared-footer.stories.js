1ea7e4bea6438cb7150546b9d5b5eb08a0b70ec3
Dan Holloran <dan@helloample.com>
2020-01-24 13:38:42 -0500

    Tweaks storybook setup

--- src/components/crds-shared-footer/crds-shared-footer.stories.js
+++ src/components/crds-shared-footer/crds-shared-footer.stories.js
@@ -3,11 +3,10 @@ import staticFooter from './static-footer';
 
 const stories = storiesOf('Footer', module);
 stories
+  .add('Static Footer', () => {
+    return staticFooter();
+  })
+  .addParameters({ percy: { skip: true } })
   .add('<crds-shared-footer />', () => {
     return `<crds-shared-footer></crds-shared-footer>`;
-  })
-  .addParameters({ percy: { skip: true } });
-
-stories.add('Static Footer', () => {
-  return staticFooter();
-});
+  });
--- src/components/crds-shared-header/shared-header.stories.js
+++ src/components/crds-shared-header/shared-header.stories.js
@@ -4,8 +4,8 @@ import { storiesOf } from '@storybook/polymer';
 const stories = storiesOf('Header', module);
 stories
   // .addDecorator(withKnobs) //
+  .addParameters({ percy: { skip: true } })
   .add('<crds-shared-header />', () => {
     // const id = text("id", "4D9pCED9eM0GOQCOKyY0mS");
     return `<crds-shared-header></crds-shared-header>`;
-  })
-  .addParameters({ percy: { skip: true } });
+  });
